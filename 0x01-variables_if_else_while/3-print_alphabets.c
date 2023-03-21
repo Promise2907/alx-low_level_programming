d11e156092e4a949b882cc862840ca12ec18b070
Promise2907 <promzyisaac3@gmail.com>
2023-03-20 18:17:38 -0700

    root@d82c2624a9d2:/alx-low_level_programming/0x01-variables_if_else_while# git commit -m

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - returns alphabets both lower and upper cases
+ * Return; Always 0 (Success)
+ */
+int main(void)
+{
+	int alp = 'a';
+	int ALP = 'A';
+
+	while (alp <= 'z')
+	{
+		putchar(alp);
+		alp = alp + 1;
+	}
+	while (ALP <= 'Z')
+	{
+		putchar(ALP);
+		ALP = ALP + 1;
+	}
+	puthar('\n');
+	return (0);
+}
